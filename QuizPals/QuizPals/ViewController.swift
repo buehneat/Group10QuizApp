ba4215eeb401e0695b688b8d9b2cc606bc753a27
buehneat <buehneat@vcu.edu>
2018-04-24 13:03:30 -0400

    Created Project in XCode

---
+++ QuizPals/QuizPals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizPals
+//
+//  Created by Aaron Buehne on 4/24/18.
+//  Copyright © 2018 group10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
