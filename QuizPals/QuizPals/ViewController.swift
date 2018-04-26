769014e637417fdec95009b748ccc548b015b8d9
buehneat <buehneat@vcu.edu>
2018-04-26 00:02:52 -0400

    Made button work for all Screen sizes

--- QuizPals/QuizPals/ViewController.swift
+++ QuizPals/QuizPals/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startQuizButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        startQuizButton.titleLabel?.adjustsFontSizeToFitWidth = true;
     }
 
     override func didReceiveMemoryWarning() {
