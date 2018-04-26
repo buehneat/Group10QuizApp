309943bcca8caa2a259666b182c0211b2a5c1566
buehneat <buehneat@vcu.edu>
2018-04-26 12:39:33 -0400

    Created swift files for both partners to work in

---
+++ QuizPals/QuizPals/BuehneWork.swift
@@ -0,0 +1,13 @@
+//
+//  BuehneWork.swift
+//  QuizPals
+//
+//  Created by Aaron Buehne on 4/26/18.
+//  Copyright © 2018 group10. All rights reserved.
+//
+
+import Foundation
+
+class BuehneWork {
+    
+}
