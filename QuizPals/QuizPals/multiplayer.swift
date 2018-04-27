537d3179c1d75a6f21bfff9fea3cfc6d30e68af5
Yashar <ya.atajan@gmail.com>
2018-04-27 17:21:10 -0400

    UI almost done for multi
    MCP code is not working but will be ifxed.

--- QuizPals/QuizPals/multiplayer.swift
+++ QuizPals/QuizPals/multiplayer.swift
@@ -10,8 +10,8 @@ import UIKit
 import CoreMotion
 import MultipeerConnectivity
 
-class multiplayer: UIViewController {
-    
+class multiplayer: UIViewController, MCBrowserViewControllerDelegate, MCSessionDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
     var session: MCSession!
     var peerID: MCPeerID!
     
@@ -21,10 +21,33 @@ class multiplayer: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.peerID = MCPeerID(displayName: UIDevice.current.name)
+        self.session = MCSession(peer: peerID)
+        self.browser = MCBrowserViewController(serviceType: "quiz", session: session)
+        self.assistant = MCAdvertiserAssistant(serviceType: "quiz", discoveryInfo: nil, session: session)
+        
+        assistant.start()
+        session.delegate = self
+        browser.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // required functions for MCBrowserViewControllerDelegate
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        // Called when the browser view controller is dismissed
+        dismiss(animated: true, completion: nil)
     }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        // Called when the browser view controller is cancelled
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // required functions for MCSessionDelegate
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+    }
+    
+    @IBAction func connect(_ sender: UIButton) {
+            present(browser, animated: true, completion: nil)
+    }
+    
 }
