0480ffaf723a48a7ca25a7fe209b14fe5a702862
Yashar <ya.atajan@gmail.com>
2018-04-27 16:19:33 -0400

    Little design update

--- QuizPals/QuizPals/multiplayer.swift
+++ QuizPals/QuizPals/multiplayer.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreMotion
+import MultipeerConnectivity
 
 class multiplayer: UIViewController {
 
@@ -20,16 +22,4 @@ class multiplayer: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
