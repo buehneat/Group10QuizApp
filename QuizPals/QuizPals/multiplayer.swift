d074c9dd3c0e640e5aa4fd57796267953034158a
Yashar <ya.atajan@gmail.com>
2018-04-27 17:38:35 -0400

    error fix

--- QuizPals/QuizPals/multiplayer.swift
+++ QuizPals/QuizPals/multiplayer.swift
@@ -43,11 +43,51 @@ class multiplayer: UIViewController, MCBrowserViewControllerDelegate, MCSessionD
     }
     
     // required functions for MCSessionDelegate
-    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {}
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+        print("inside didReceiveData")
+        
+        // this needs to be run on the main thread
+        DispatchQueue.main.async(execute: {
+//            if let receivedChoice = NSKeyedUnarchiver.unarchiveObject(with: data) as? String{
+//                //TODO: Update the opponents choice in here
+//            }
+        })
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {}
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {}
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        // Called when a connected peer changes state (for example, goes offline)
+        switch state {
+        case MCSessionState.connected:
+            print("Connected: \(peerID.displayName)")
+
+        case MCSessionState.connecting:
+            print("Connecting: \(peerID.displayName)")
+
+        case MCSessionState.notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        }
     }
     
     @IBAction func connect(_ sender: UIButton) {
             present(browser, animated: true, completion: nil)
     }
-    
+
+    @IBAction func sendChoice(_ sender: UIButton) {
+//        let choice = //TODO: Add button choice here
+//        let dataToSend =  NSKeyedArchiver.archivedData(withRootObject: msg!)
+//        do{
+//            try session.send(dataToSend, toPeers: session.connectedPeers, with: .unreliable)
+//        }
+//        catch let err {
+//            //print("Error in sending data \(err)")
+//        }
+//        updateChatView(newText: msg!, id: peerID)
+    }
 }
